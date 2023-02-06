70f18541ccb98e8e651db729777776f3ea0c7e9d
Turmoil <tony-turmoil@qq.com>
2023-02-06 21:56:14 +0800

    command

---
+++ Command/ButtonCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Command
+{
+	public class ButtonCommand : ICommand
+	{
+		private Action _executeAction;
+
+		public ButtonCommand(Action action)
+		{
+			_executeAction = action;
+		}
+
+		public event EventHandler? CanExecuteChanged;
+
+		public bool CanExecute(object? parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object? parameter)
+		{
+			_executeAction();
+		}
+	}
+}
---
+++ Command/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Command
+{
+	public class MainViewModel
+	{
+		public MainViewModel()
+		{
+			ShowCommand = new ButtonCommand(Show);
+		}
+
+		// Must be public, not private.
+		public ButtonCommand ShowCommand { get; set; }
+		
+		public void Show()
+		{
+			MessageBox.Show("You have clicked the button.");
+		}
+	}
+}
