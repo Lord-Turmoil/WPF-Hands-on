e3aafe6175bcafd595c7bb2dc81bd2670bc91c09
Turmoil <tony-turmoil@qq.com>
2023-02-07 13:01:00 +0800

    notify

---
+++ Notify/CommandForward.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Notify
+{
+	public class CommandForward : ICommand
+	{
+		public event EventHandler? CanExecuteChanged;
+
+		private Action _executeAction;
+
+        public CommandForward(Action action)
+        {
+			_executeAction = action;
+        }
+
+        public bool CanExecute(object? parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object? parameter)
+		{
+			_executeAction();
+		}
+	}
+}
---
+++ Notify/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Notify
+{
+	public class MainViewModel : ViewModelBase
+	{
+
+		private string? _name;
+
+		public CommandForward ClickCommand { get; set; }
+
+        public MainViewModel()
+        {
+			Name = "Hello there!";
+			ClickCommand = new CommandForward(OnClickAction);
+        }
+
+        public string Name
+		{
+			get { return _name; }
+			set
+			{
+				_name = value;
+
+				// argument should be the value that the UI binds.
+				// OnPropertyChanged("Name");
+				OnPropertyChanged();	// Auto filled by complier due to [CallerMemberName]
+			}
+		}
+
+		public void OnClickAction()
+		{
+			Name = "Click OK";
+			MessageBox.Show("You have clicked the button.");
+			Name = "General Kenobi!";
+		}
+	}
+}
